c04926b5748353846a82b66ff7b36d9b2a4a8575
enthye <37763349+enthye@users.noreply.github.com>
2019-04-03 16:48:41 +1300

    basic design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-dark text-light">
+            <a className="navbar-brand" href="/">Kevin Xu</a>
+
+            <button className="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+
+          <svg width="100%" height="320">
+              <circle cx="50%" cy="50%" r="100" fill="white"/>
+          </svg>
+
+          <h2 align="center">
+            Inspired Solutions Driven Design
+          </h2>
+
+          <p align="center">Website strongly inspired by movio.co</p>
+
       </div>
     );
   }
